f1adbcf953790c7d944914716033d7501d7a14ae
joostibor <43845789+joostibor@users.noreply.github.com>
2020-10-27 15:02:25 +0000

    Update pages to output generated at 9c44160

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,4 +1,7 @@
 var searchData=
 [
+  ['parsefile',['parseFile',['../classjsonParser.html#a5ef05d02e97fe159b254e58b92c5794f',1,'jsonParser']]],
+  ['parsestream',['parseStream',['../classjsonParser.html#a7e16d0e2c697b0dda37599d36dc6300d',1,'jsonParser']]],
+  ['parsestring',['parseString',['../classjsonParser.html#ae7c68d90df9fe992e7d8c41957e00035',1,'jsonParser']]],
   ['parseunit',['parseUnit',['../classHero.html#aa3f51171514dc70e3da5d75cfaab157b',1,'Hero::parseUnit(const std::string &amp;s)'],['../classHero.html#a592b00214fda514e490d40fcc6d8bcd9',1,'Hero::parseUnit(std::istream &amp;stream)']]]
 ];
