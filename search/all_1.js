0f242ed3156020d920f34a538d995821cb72406e
joostibor <43845789+joostibor@users.noreply.github.com>
2020-11-02 19:54:47 +0000

    Update pages to output generated at 964c162

--- search/all_1.js
+++ search/all_1.js
@@ -1,9 +1,4 @@
 var searchData=
 [
-  ['getacthp',['getActHp',['../classHero.html#adf3672a72509e78d6288e3e3de5f1fe5',1,'Hero']]],
-  ['getatkspeed',['getAtkspeed',['../classHero.html#ae66ea13f38d13fd5ed3b2fb5b0f43017',1,'Hero']]],
-  ['getattack',['getAttack',['../classHero.html#aaed8c9484ac81d4004667f9064e6e9b4',1,'Hero']]],
-  ['getdmg',['getDmg',['../classHero.html#a60e7b5be59457128423fee62b254721f',1,'Hero']]],
-  ['getmaxhp',['getMaxHp',['../classHero.html#aac4fbca6752b73b99f7af5023489a9c5',1,'Hero']]],
-  ['getxp',['getXp',['../classHero.html#a2477a58e24f6cd1e330c694d68594a94',1,'Hero']]]
+  ['count',['count',['../classJSON.html#aa95c8567c7aa2e21dc93a9d4edbd6d32',1,'JSON']]]
 ];
---
+++ search/all_9.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['name',['name',['../classMonster.html#a4ce38cc9d6af5a37f54f90d2d7ab8ee0',1,'Monster']]],
+  ['nextattack',['nextAttack',['../classMonster.html#ad93c37b981f2d8c55ff0e3c6f5515e2f',1,'Monster']]]
+];
