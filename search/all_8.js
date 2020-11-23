3966f117ba4d6ad6bbed325ac6535acbed32878e
joostibor <43845789+joostibor@users.noreply.github.com>
2020-11-23 19:43:14 +0100

    Update pages to output generated at 03914ac

--- search/all_8.js
+++ search/all_8.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['map_27',['Map',['../classMap.html',1,'']]],
   ['maxhp_28',['maxhp',['../classMonster.html#a3935c40431f7f6dbdf0a4729c551679b',1,'Monster']]],
-  ['monster_29',['monster',['../structGame_1_1monster.html',1,'Game::monster'],['../classMonster.html',1,'Monster'],['../classMonster.html#a143f8f1462ee70375460dc3f7a573e51',1,'Monster::Monster()']]]
+  ['monster_29',['monster',['../structGame_1_1monster.html',1,'Game::monster'],['../classMonster.html',1,'Monster'],['../classMonster.html#a09efc96cb4d304a6eb55343d5f028b1e',1,'Monster::Monster()']]]
 ];
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['monster_76',['Monster',['../classMonster.html#a143f8f1462ee70375460dc3f7a573e51',1,'Monster']]]
+  ['monster_76',['Monster',['../classMonster.html#a09efc96cb4d304a6eb55343d5f028b1e',1,'Monster']]]
 ];
